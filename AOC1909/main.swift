665cdb05fb9f1de8614aae52dd392ffed17a9800
Heiko Goes <heiko.goes@gaebler-goes.de>
2019-12-20 05:31:05 +0100

    Initial Commit

---
+++ AOC1909/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AOC1909
+//
+//  Created by Heiko Goes on 20.12.19.
+//  Copyright © 2019 Heiko Goes. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
